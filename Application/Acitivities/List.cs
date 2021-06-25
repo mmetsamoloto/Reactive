1d43069eb76dce6707f205e98d58ce510341b640
mmetsa moloto <mmetsamoloto@gmail.com>
2021-06-25 14:53:22 +0200

    End of Folder 4

--- Application/Acitivities/List.cs
+++ Application/Acitivities/List.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Acitivities
@@ -15,14 +17,16 @@ namespace Application.Acitivities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             public DataContext Context { get; }
-            public Handler(DataContext context)
+
+            public Handler(DataContext context, ILogger<List> logger)
             {
+
                 this.Context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await Context.Activities.ToListAsync();
+                return await Context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
